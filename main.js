182e621063841595017d87389918841d52c507e2
sivaaravindh <sivaaravind262003@gmail.com>
2023-01-15 12:05:32 +0530

    index

---
+++ main.js
@@ -0,0 +1,28 @@
+
+let age=parseInt(prompt("enter your age"));
+
+// if(18<=age ){
+
+// 	if ( confirm("liscense proof ")) {
+	 
+//  	 console.log( "you go the eligible");
+// 	}
+
+// 	else {
+// 		console.log( "your  not  to diving eligible");
+// 	 }
+// }
+
+
+if(18<=age && confirm("liscense proof ")){
+
+ 	 console.log( "you go the dirive");
+}
+
+else {
+	console.log( "your  not  to diving eligible");
+ }
+
+
+
+
